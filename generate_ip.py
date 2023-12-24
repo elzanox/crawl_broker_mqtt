3185e8976c1a7947410b83568c0dab5d1d151f1e
Immanuel <dimas@admin.spacearcade.online>
2023-12-24 19:47:43 +0700

    add

---
+++ generate_ip.py
@@ -0,0 +1,83 @@
+import csv
+import random
+import time
+import os
+
+# Rentang alamat IP yang diinginkan
+start_ip = "218.234.0.0"
+end_ip = "218.235.255.255"
+output_csv = f'data/{start_ip}-{end_ip}.csv'
+# Mengecek apakah file sudah ada
+if not os.path.exists(output_csv):
+    # Jika file tidak ada, maka kita membuatnya
+    with open(output_csv, mode='w', newline='') as file_csv:
+        # Membuat objek writer
+        writer = csv.writer(file_csv)
+        # Tidak ada penulisan data dalam contoh ini
+    print(f"File CSV kosong dengan nama '{output_csv}' berhasil dibuat.")
+else:
+    print(f"File CSV dengan nama '{output_csv}' sudah ada. Tidak perlu membuat baru.")
+    
+def generate_ip_in_range(start_ip, end_ip):
+    start_parts = list(map(int, start_ip.split('.')))
+    end_parts = list(map(int, end_ip.split('.')))
+
+    # Konversi IP awal dan akhir ke bentuk numerik
+    start_num = (start_parts[0] << 24) + (start_parts[1] << 16) + (start_parts[2] << 8) + start_parts[3]
+    end_num = (end_parts[0] << 24) + (end_parts[1] << 16) + (end_parts[2] << 8) + end_parts[3]
+
+    # Menghasilkan alamat IP acak dalam rentang yang ditentukan
+    random_num = random.randint(start_num, end_num)
+
+    # Konversi angka acak kembali ke bentuk alamat IP
+    random_ip_parts = [
+        (random_num >> 24) & 0xFF,
+        (random_num >> 16) & 0xFF,
+        (random_num >> 8) & 0xFF,
+        random_num & 0xFF
+    ]
+
+    ip_adress = ".".join(map(str, random_ip_parts))
+    return ip_adress
+
+def generate_ip_random():
+    # Menghasilkan empat bagian angka antara 0 dan 255
+    ip_parts = [random.randint(0, 255) for _ in range(4)]
+    
+    # Menggabungkan bagian-bagian IP menjadi satu string
+    ip_address = '.'.join(map(str, ip_parts))
+    
+    return ip_address
+
+def is_ip_exists(ip, filename=output_csv):
+    # Mengecek apakah IP sudah ada dalam file CSV
+    with open(filename, mode='r') as file:
+        reader = csv.reader(file)
+        # Mengecek setiap baris dalam file
+        for row in reader:
+            if row and row[0] == ip:
+                return True
+    return False
+
+def save_to_csv(ip, filename=output_csv):
+    # Menyimpan alamat IP ke dalam file CSV
+    with open(filename, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        # Menulis alamat IP baru
+        writer.writerow([ip])
+try:
+    while True:
+        new_ip = generate_ip_in_range(start_ip,end_ip)
+
+        while is_ip_exists(new_ip):
+            print(f"IP {new_ip} sudah ada dalam file CSV, menghasilkan IP baru...")
+            new_ip = generate_ip_in_range(start_ip,end_ip)
+
+        save_to_csv(new_ip)
+        print(f"IP Address {new_ip} telah disimpan dalam file {output_csv}.")
+
+        # Menunggu selama 1 detik sebelum menghasilkan IP berikutnya
+        # time.sleep(0.1)
+
+except KeyboardInterrupt:
+    print("\nProgram dihentikan.")
---
+++ search_ip_broker.py
@@ -0,0 +1,82 @@
+import paho.mqtt.client as mqtt
+import pandas as pd
+from tqdm import tqdm
+import os
+import csv
+# Ganti dengan nama file CSV yang sesuai
+# csv_file = 'result.csv'
+csv_file = 'data/218.234.0.0-218.235.255.255.csv'
+headerr = ['ip_address','status','mqtt']
+# Ganti dengan nama kolom yang ingin dibaca
+target_column_name = 'ip_address'  # Contoh: membaca kolom dengan nama 'nama_kolom'
+
+if not os.path.exists(csv_file):
+    # Jika file tidak ada, maka kita membuatnya
+    with open(csv_file, mode='w', newline='') as file_csv:
+        # Membuat objek writer
+        writer = csv.writer(file_csv)
+        # Menulis data ke dalam file CSV
+        writer.writerows(headerr)
+        # Tidak ada penulisan data dalam contoh ini
+    print(f"File CSV kosong dengan nama '{csv_file}' berhasil dibuat.")
+else:
+    print(f"File CSV dengan nama '{csv_file}' sudah ada. Tidak perlu membuat baru.")
+
+
+
+def test_mqtt(ip, port):
+    try:
+        client = mqtt.Client()
+        client.connect(ip, port, 60)
+        print(f"Berhasil terhubung ke {ip}")
+        client.disconnect()  # Memutuskan koneksi setelah berhasil terhubung
+        return "berhasil"
+    except Exception as e:
+        print(f"IP Broker: {ip} Gagal, Error: {e}")
+        return "gagal"
+        
+# Fungsi yang dipanggil ketika koneksi ke broker berhasil dibuat
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Koneksi ke broker berhasil.")
+        # Subscribe pada topik tertentu setelah koneksi berhasil
+        client.subscribe("#")
+    else:
+        print(f"Gagal terhubung ke broker dengan kode {rc}")
+
+# Fungsi yang dipanggil ketika pesan diterima dari broker
+def on_message(client, userdata, msg):
+    print(f"Pesan diterima dari topik {msg.topic}: {msg.payload.decode()}")
+
+# Inisialisasi objek client MQTT
+client = mqtt.Client()
+
+# Menetapkan fungsi callback ketika koneksi berhasil dibuat
+client.on_connect = on_connect
+
+# Menetapkan fungsi callback ketika pesan diterima
+client.on_message = on_message
+
+# Tentukan alamat broker dan port yang digunakan
+# broker_address = "3.123.112.129"
+port = 1883
+
+# Membaca file CSV ke dalam DataFrame
+df = pd.read_csv(csv_file)
+ip_list = df['ip_address'].tolist()
+# Terhubung ke broker menggunakan alamat dan port yang telah ditentukan
+for ip in tqdm(df['ip_address'], desc="Testing IPs"):
+    index = ip_list.index(ip)
+    print(f"Mencoba: {index} {ip}")
+    mqtt_val = test_mqtt(ip,port)
+    # status_val = test_ping(ip)
+# for index, ip in tqdm(enumerate(df['ip_address'][10937:], start=10937), desc="Testing IPs"):
+#     index = ip_list.index(ip)
+#     print(f"Mencoba: {index} {ip}")
+#     mqtt_val = test_mqtt(ip,port)
+#     # status_val = test_ping(ip)
+    df.at[index, 'mqtt'] = mqtt_val
+    df.to_csv(csv_file, index=False)
+
+# Loop tak terhingga untuk menjaga koneksi tetap hidup
+# client.loop_forever()
